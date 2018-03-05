8ede98788780e92da91bf0645647bfb043fa3cef
22498 <22498@ma-web.nl>
2018-03-05 11:12:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Opdracht3 - Max Verhaagen</title>
+
+</head>
+
+<body>
+
+<?php
+
+// lokaal 2.13
+
+$voornaam = 'Max';
+$leeftijd = 17;
+
+if ($leeftijd >= 18) {
+    echo 'Jij bent oud genoeg om te drinken.';
+} else {
+    echo  'Jij bent te jong om te drinken';
+
+}
+
+?>
+
+</body>
+</html>
+
